da899517e4ec710d13d9eb155c4d8d045770206f
Adam Bartniczak <adam.bartniczak@agora.pl>
2017-03-23 11:19:23 +0100

    middleware

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
-var http = require('http');
-var server = http.createServer(function (req, res) {
-  res.end('hello world');
+/*
+ * bez EXPRESS
+ */
+// var http = require('http');
+// var server = http.createServer(function (req, res) {
+//   res.end('hello world');
+// });
+// server.listen(3000, function () {
+//   console.log("When this callback is invoked our server is listening on port: " + 3000);
+// });
+
+/*
+ * z EXPRESS
+ */
+var express = require('express');
+var app = express();
+
+function logRequest(req, res, next) {
+    console.log('incoming request at ', new Date());
+    next();
+};
+
+app.use(logRequest);
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
 });
-server.listen(3000, function () {
-  console.log("When this callback is invoked our server is listening on port: " + 3000);
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
 });
