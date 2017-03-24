5347f1bfb2dcaa1756cbd8e21487097124f52031
Adam Bartniczak <adam.bartniczak@agora.pl>
2017-03-24 11:23:38 +0100

    app

---
+++ readme.txt
@@ -0,0 +1,4 @@
+npm config set save-exact true              - wylacza dodawanie wersjonowania dla modulow node (^ i ~)
+
+Polecenie 'mongod' uruchamiac w konsoli z prawami admina!
+
--- test/e2eInventoryTest.js
+++ test/e2eInventoryTest.js
@@ -1,11 +1,12 @@
-const request = require('supertest');
-const app = require('../index');
+var request = require('supertest');
+var inMemoryStockRepository = require('../src/inMemoryStockRepository')();
+var app = require('../src/index')(inMemoryStockRepository);
 
 describe('Book inventory', function () {
     it('allows to stock up the items', function (done) {
         request(app)
             .post('/stock')
-            .send({ isbn: '1213214334324', count: 10 })
-            .expect({ isbn: '1213214334324', count: 10 }, done); // 'done' dla kodu asynchronicznego - patrz tez argument funkcji
+            .send({ isbn: '0123456789', count: 10 })
+            .expect({ isbn: '0123456789', count: 10 }, done);
     });
 });
